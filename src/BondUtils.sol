98315dc0b4b0b3f4c72f697e8cbd392b6e3c534f
frehml <frederic.hamelink@hotmail.com>
2024-10-05 14:47:50 +0200

    change tohex function

--- src/BondUtils.sol
+++ src/BondUtils.sol
@@ -256,14 +256,13 @@ contract BondUtils {
         currencyAmount = currencyAmountToBond;
     }
 
-    // XXX TODO change to bitwise operators
     function toHex(bytes memory buffer) public pure returns (string memory) {
         bytes memory converted = new bytes(buffer.length * 2);
         bytes memory _base = "0123456789abcdef";
 
         for (uint256 i = 0; i < buffer.length; i++) {
-            converted[i * 2] = _base[uint8(buffer[i]) / _base.length];
-            converted[i * 2 + 1] = _base[uint8(buffer[i]) % _base.length];
+            converted[i * 2] = _base[uint8(buffer[i] >> 4)]; // Get the high nibble
+            converted[i * 2 + 1] = _base[uint8(buffer[i] & 0x0f)]; // Get the low nibble
         }
 
         return string(converted);
