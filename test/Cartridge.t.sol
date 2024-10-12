e5b98e082f177e18d3847c71d3a595d7e7facd68
frehml <frederic.hamelink@hotmail.com>
2024-10-12 14:43:53 +0200

    set up cartridge test

--- test/Cartridge.t.sol
+++ test/Cartridge.t.sol
@@ -2,5 +2,28 @@
 pragma solidity >=0.8.24;
 
 import "@std/Test.sol";
+import {Cartridge} from "../src/Cartridge.sol";
+import {CartridgeBondUtils} from "../src/CartridgeBondUtils.sol";
 
-contract CartridgeTest is Test {}
+contract CartridgeTest is Test {
+    Cartridge cartridge;
+    CartridgeBondUtils cartridgeBondUtils;
+    address operator = address(1);
+    uint256 maxSteps = 100;
+
+    function setUp() public {
+        cartridgeBondUtils = new CartridgeBondUtils();
+
+        cartridge = new Cartridge(operator, address(cartridgeBondUtils), maxSteps);
+    }
+
+    function testCartridgeDeployment() public view {
+        uint256 codeSize;
+        address cartridgeAddress = address(cartridge);
+        assembly {
+            codeSize := extcodesize(cartridgeAddress)
+        }
+
+        assertTrue(codeSize > 0, "Cartridge should be deployed as a contract");
+    }
+}
